b4fd43a530523dfcad4da6dbda283a7039169d62
rahul bs <rbs9711@gmail.com>
2019-09-30 12:20:42 +0530

    added author variable and setter method

--- Book.java
+++ Book.java
@@ -2,10 +2,13 @@ class book{
 
 private int bookId;
 private String name;
+private String author;
 
-public void getBookId(){
+public void getBookId(){}
 }
 public void setBookId(){
 	System.out.println("bookid is set");
 }
+public void setAuthorId(){
 }
+
